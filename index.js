ea7d650502a0877b16d9daa85707ef589b6d3705
Daniela Fajardo <danielafajardo2315@gmail.com>
2025-08-22 16:59:12 -0500

    Se agregan locaciones de los pokemon para cada tarjeta

--- index.js
+++ index.js
@@ -40,11 +40,17 @@ document.addEventListener('DOMContentLoaded', () => {
             const imagenPokemon = datosPokemon.sprites.other.dream_world.front_default;
             const namePokemon = datosPokemon.name;
             const typePokemon = datosPokemon.types.map(tipo => tipo.type.name);
+            const locationsUrl = datosPokemon.location_area_encounters;
+            const locationRes = await fetch(locationsUrl);
+            const location = await locationRes.json();
+            const locationPokemon = await location.map(loc => loc.location_area);
+            const locPokemon = locationPokemon.map(loc => loc.name);
 
+            console.log(locPokemon);
             console.log(namePokemon);
             console.log(imagenPokemon);
-            const colorTipo = { 
-                'bug': '#A8B820', 
+            const colorTipo = {
+                'bug': '#A8B820',
                 'dark': '#705848',
                 'dragon': '#7039F5',
                 'electric': '#E3C033',
@@ -53,7 +59,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 'fire': '#F05030',
                 'flying': '#A890F0',
                 'ghost': '#705898',
-                'grass': '#79BC55', 
+                'grass': '#79BC55',
                 'ground': '#E0C068',
                 'ice': '#98D8D8',
                 'normal': '#A8A878',
@@ -67,17 +73,33 @@ document.addEventListener('DOMContentLoaded', () => {
                 const color = colorTipo[tipo];
                 return `<button class="btn_type" style="background-color: ${color};">${tipo}</button>`;
             }).join('');
+            const locationList = locPokemon.map(loc => {
+                const locationP = locPokemon[loc];
+                return `<ul>
+                <li class="list_loc">${loc}</li>
+                </ul>`;
+            }).join('');
 
-            infoPokemon.innerHTML = `
-        <article class="card">
-        <h2 class="card_title">${namePokemon}</h2>
-        <div class="imgContainer">
-        <img class="img_pokemon" src="${imagenPokemon}" alt="Imagen ${namePokemon}" id="img_pokemon">
-        </div>
-        <div>${botonesTipo}</div>
-        </article>
-        `
-        
+            infoPokemon.innerHTML += `
+                <article class="card">
+                <h2 class="card_title">${namePokemon}</h2>
+                <div class="imgContainer">
+                <img class="img_pokemon" src="${imagenPokemon}" alt="Imagen ${namePokemon}" id="img_pokemon">
+                </div>
+                <div>${botonesTipo}</div>
+                <div>
+                <h3>Locations</h3>
+                <ul>
+                <li class="no_evolucion">Esta es una evolución</li>
+                ${locationList}
+                </ul></div>
+                </article>
+                `
+            if (!locationList.length){
+                const evolucion = document.querySelector('.no_evolucion');
+                evolucion.style.display = 'block';
+            }
+
         } catch (error) {
             console.error('hay un error', error);
         }
